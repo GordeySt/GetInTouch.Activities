49e5fd53ad7c706b4711cdef70dcf842f7178964
gordeyst <gordeystahovets@gmail.com>
2021-01-30 21:01:11 +0300

    Create initial seed if Activities Set is empty

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -6,7 +6,7 @@ namespace Domain
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Desription { get; set; }
+        public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
