879b7a94c173fb6df9cd449ee3cbfd74499a7eaf
gordeyst <gordeystahovets@gmail.com>
2021-02-02 16:49:02 +0300

    Implement ActivityDashboard component

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { List, Container } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import { NavBar } from "../../Features/nav/NavBar";
+import { ActivityDashboard } from "../../Features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -19,11 +20,7 @@ const App = () => {
     <React.Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <List>
-          {activities.map((activity: IActivity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities}/>
       </Container>
     </React.Fragment>
   );
