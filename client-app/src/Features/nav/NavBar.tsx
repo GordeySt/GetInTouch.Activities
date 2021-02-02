3b72087f43b8028a107a12a49a16e307abfaaa53
gordeyst <gordeystahovets@gmail.com>
2021-02-02 16:29:09 +0300

    Add logo to NavBar

--- client-app/src/Features/nav/NavBar.tsx
+++ client-app/src/Features/nav/NavBar.tsx
@@ -1,11 +1,22 @@
-import { Menu } from "semantic-ui-react";
+import { Menu, Container, Button } from "semantic-ui-react";
 
 export const NavBar = () => {
   return (
-    <Menu inverted>
-      <Menu.Item name="home" />
-      <Menu.Item name="messages" />
-      <Menu.Item name="friends" />
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item hedaer>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          InTouch
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
     </Menu>
   );
 };
