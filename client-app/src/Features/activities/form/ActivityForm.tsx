ac008b9adebd4abf23aff2b5cd35a121d40960dc
gordeyst <gordeystahovets@gmail.com>
2021-02-02 18:40:50 +0300

    Add submit and cancel button to form

--- client-app/src/Features/activities/form/ActivityForm.tsx
+++ client-app/src/Features/activities/form/ActivityForm.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Segment, Form } from 'semantic-ui-react'
+import React from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
 
 export const ActivityForm = () => {
-    return (
-        <Segment>
-            <Form>
-                <Form.Input placeholder='Title' />
-                <Form.TextArea rows={2} placeholder='Description' />
-                <Form.Input placeholder='Category' />
-                <Form.Input type='date' placeholder='Date' />
-                <Form.Input placeholder='City' />
-                <Form.Input placeholder='Venue' />
-            </Form>
-        </Segment>
-    )
-}
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder="Title" />
+        <Form.TextArea rows={2} placeholder="Description" />
+        <Form.Input placeholder="Category" />
+        <Form.Input type="date" placeholder="Date" />
+        <Form.Input placeholder="City" />
+        <Form.Input placeholder="Venue" />
+        <Button floated="right" inverted secondary content="Submit" />
+        <Button floated="right" inverted color="red" content="Cancel" />
+      </Form>
+    </Segment>
+  );
+};
