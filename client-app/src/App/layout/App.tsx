3c5862d4d170824f2dbb3130207156c2c373600e
gordeyst <gordeystahovets@gmail.com>
2021-02-03 22:44:43 +0300

    Add new created activity to the DB

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -26,9 +26,11 @@ const App = () => {
   };
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+    Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    });
   };
 
   const handleEditActivity = (activity: IActivity) => {
