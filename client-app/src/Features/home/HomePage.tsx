e17b321a4c502fb29d41cbd1131131fc02b99a63
gordeyst <gordeystahovets@gmail.com>
2021-02-07 14:36:26 +0300

    Change home page

--- client-app/src/Features/home/HomePage.tsx
+++ client-app/src/Features/home/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 export const HomePage = () => {
   return (
     <Container style={{ marginTop: "7em" }}>
       <h1>Home Page</h1>
+      <h4>
+        Go to <Link to="/activities">Activities</Link>
+      </h4>
     </Container>
   );
 };
