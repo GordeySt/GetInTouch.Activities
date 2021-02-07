dff98fe41565ec484eab576243b9463d62772d25
gordeyst <gordeystahovets@gmail.com>
2021-02-07 17:16:32 +0300

    Group activities with the same date

--- client-app/src/Features/activities/dashboard/ActivityList.tsx
+++ client-app/src/Features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,28 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { Segment, Item } from "semantic-ui-react";
+import { Segment, Item, Label } from "semantic-ui-react";
 import { IActivity } from "../../../App/models/activity";
 import ActivityStore from "../../../App/stores/ActivityStore";
-import { ActivityListItem } from "./ActivityListItem"
+import { ActivityListItem } from "./ActivityListItem";
 
 export const ActivityList: React.FC = observer(() => {
-  const {
-    activitiesByDate,
-  } = ActivityStore;
+  const { activitiesByDate } = ActivityStore;
   return (
-    <Segment clearing>
-      <Item.Group divided>
-        {activitiesByDate.map((activity: IActivity) => (
-          <ActivityListItem key={activity.id} activity={activity} />
-        ))}
-      </Item.Group>
-    </Segment>
+    <React.Fragment>
+      {activitiesByDate.map(([group, activities]) => (
+        <React.Fragment key={group}>
+          <Label circular color="black" size="large">
+            {group}
+          </Label>
+          <Segment clearing>
+            <Item.Group divided>
+              {activities.map((activity: IActivity) => (
+                <ActivityListItem key={activity.id} activity={activity} />
+              ))}
+            </Item.Group>
+          </Segment>
+        </React.Fragment>
+      ))}
+    </React.Fragment>
   );
 });
