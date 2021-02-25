c03bb713120416f86631ec2c703c0cac2441f6fb
gordeyst <gordeystahovets@gmail.com>
2021-02-25 10:17:46 +0300

    Redesign home page

--- client-app/src/Features/home/HomePage.tsx
+++ client-app/src/Features/home/HomePage.tsx
@@ -1,45 +1,58 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Segment,
+} from "semantic-ui-react";
 import UserStore from "../../App/stores/UserStore";
+import { LoginForm } from "../user/LoginForm";
 
 export const HomePage = () => {
   const { isLoggedIn, user } = UserStore;
   return (
-    <Segment inverted textAlign="center" vertical className="masthead">
-      <Container text>
-        <Header as="h1" inverted>
-          <Image
-            size="massive"
-            src="/assets/logo.png"
-            alt="logo"
-            style={{ marginBottom: 12 }}
-          />
-          Activities
-        </Header>
+    <Fragment>
+      <Segment inverted textAlign="center" vertical className="masthead">
         {isLoggedIn && user ? (
-          <Fragment>
+          <Container text>
+            <Header as="h2" color="black">
+              <Icon name="users" />
+            </Header>
             <Header
+              color="black"
               as="h2"
-              inverted
-              content={`Welcome back, ${user.displayedName}`}
+              content={`Welcome to Activities, ${user?.displayedName}`}
             />
-            <Button as={Link} to="/activities" size="huge" inverted>
-              Go to Activities
+            <Header color="black" as="h2" content="Bring people together" />
+            <Button color="black" as={Link} to="/activities" size="huge">
+              Let's get it started
             </Button>
-          </Fragment>
+          </Container>
         ) : (
-          <Fragment>
-            <Header as="h2" inverted content={"Bring people together"} />
-            <Button as={Link} to="/login" size="huge" inverted>
-              Login
-            </Button>
-            <Button as={Link} to="/register" size="huge" inverted>
-              Register
-            </Button>
-          </Fragment>
+          <Container text>
+            <Fragment>
+              <Segment placeholder>
+                <Grid columns={2} relaxed="very" stackable>
+                  <Grid.Column>
+                    <LoginForm />
+                  </Grid.Column>
+
+                  <Grid.Column verticalAlign="middle">
+                    <Button content="Sign up" icon="signup" size="big" />
+                  </Grid.Column>
+                </Grid>
+
+                <Divider vertical>Or</Divider>
+              </Segment>
+            </Fragment>
+          </Container>
         )}
-      </Container>
-    </Segment>
+      </Segment>
+    </Fragment>
   );
 };
--- client-app/src/Features/user/LoginForm.tsx
+++ client-app/src/Features/user/LoginForm.tsx
@@ -2,7 +2,7 @@ import { FORM_ERROR } from "final-form";
 import React from "react";
 import { Form as FinalForm, Field } from "react-final-form";
 import { combineValidators, isRequired } from "revalidate";
-import { Form, Button, Label } from "semantic-ui-react";
+import { Form, Button, Label, Segment } from "semantic-ui-react";
 import { TextInput } from "../../App/common/form/TextInput";
 import { IUserFromValues } from "../../App/models/user";
 import UserStore from "../../App/stores/UserStore";
@@ -15,42 +15,43 @@ const validate = combineValidators({
 export const LoginForm = () => {
   const { login } = UserStore;
   return (
-    <FinalForm
-      onSubmit={(values: IUserFromValues) =>
-        login(values).catch((error) => ({
-          [FORM_ERROR]: error,
-        }))
-      }
-      validate={validate}
-      render={({
-        handleSubmit,
-        submitting,
-        form,
-        submitError,
-        invalid,
-        pristine,
-        dirtySinceLastSubmit,
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <Field name="email" component={TextInput} placeholder="Email" />
-          <Field
-            name="password"
-            component={TextInput}
-            placeholder="Password"
-            type="password"
-          />
-          {submitError && !dirtySinceLastSubmit && (
-            <Label color="red" basic content={submitError.statusText} />
-          )}
-          <Button
-            disabled={(invalid && !dirtySinceLastSubmit) || pristine}
-            loading={submitting}
-            positive
-            content="Login"
-          />
-          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
-        </Form>
-      )}
-    />
+    <Segment clearing>
+      <FinalForm
+        onSubmit={(values: IUserFromValues) =>
+          login(values).catch((error) => ({
+            [FORM_ERROR]: error,
+          }))
+        }
+        validate={validate}
+        render={({
+          handleSubmit,
+          submitting,
+          form,
+          submitError,
+          invalid,
+          pristine,
+          dirtySinceLastSubmit,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Field name="email" component={TextInput} placeholder="Email" />
+            <Field
+              name="password"
+              component={TextInput}
+              placeholder="Password"
+              type="password"
+            />
+            {submitError && !dirtySinceLastSubmit && (
+              <Label color="red" basic content={submitError.statusText} />
+            )}
+            <Button
+              disabled={(invalid && !dirtySinceLastSubmit) || pristine}
+              loading={submitting}
+              color="black"
+              content="Login"
+            />
+          </Form>
+        )}
+      />
+    </Segment>
   );
 };
