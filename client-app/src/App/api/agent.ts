497f8c439848a2fce01cbfde0f3deb541bc0a184
gordeyst <gordeystahovets@gmail.com>
2021-02-25 10:32:35 +0300

    Add interceptor to automatically send JWT token

--- client-app/src/App/api/agent.ts
+++ client-app/src/App/api/agent.ts
@@ -5,6 +5,16 @@ import { IUser, IUserFromValues } from "../models/user"
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+axios.interceptors.request.use((config) => {
+    const token = window.localStorage.getItem("jwt");
+    
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+}, error => {
+    return Promise.reject(error);
+})
+
 axios.interceptors.response.use(undefined, er => {
     ErrorsHandler.handleNetworkError(er);
 
