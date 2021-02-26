33cc5d0e6a598333e9aecee68594f785c04d905e
gordeyst <gordeystahovets@gmail.com>
2021-02-26 21:42:12 +0300

    Code refactoring

--- Application/Activities/CreateActivity.cs
+++ Application/Activities/CreateActivity.cs
@@ -5,12 +5,14 @@ using System.Threading;
 using Persistance;
 using Domain;
 using FluentValidation;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Activities
 {
     public class CreateActivity
     {
-        public class Command : IRequest 
+        public class Command : IRequest
         {
             public Guid Id { get; set; }
             public string Title { get; set; }
@@ -34,16 +36,38 @@ namespace Application.Activities
             }
         }
 
-        public class Handler : IRequestHandler<Command> 
+        public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context) 
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = CreateNewActivity(request);
+
+                AddNewActivityToDatabase(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetCurrentUserName());
+
+                var attendee = CreateNewAttendee(user, activity);
+
+                AddNewAttendeeToDatabase(attendee);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+
+            private Activity CreateNewActivity(Command request)
             {
                 var activity = new Activity
                 {
@@ -56,12 +80,30 @@ namespace Application.Activities
                     Venue = request.Venue
                 };
 
+                return activity;
+            }
+
+            private void AddNewActivityToDatabase(Activity activity)
+            {
                 _context.Activities.Add(activity);
-                var success = await _context.SaveChangesAsync() > 0;
+            }
 
-                if (success) return Unit.Value;
+            private UserActivity CreateNewAttendee(AppUser user, Activity activity)
+            {
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateToJoined = DateTime.Now
+                };
 
-                throw new Exception("Problem saving changes");
+                return attendee;
+            }
+
+            private void AddNewAttendeeToDatabase(UserActivity attendee)
+            {
+                _context.UserActivities.Add(attendee);
             }
         }
     }
