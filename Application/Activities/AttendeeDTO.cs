0312d69c807f963759ab583b9d110a9d3c2b000c
gordeyst <gordeystahovets@gmail.com>
2021-02-28 13:54:27 +0300

    Fix mapping for attendees

--- Application/Activities/AttendeeDTO.cs
+++ Application/Activities/AttendeeDTO.cs
@@ -2,7 +2,7 @@ namespace Application.Activities
 {
     public class AttendeeDto
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string DisplayedName { get; set; }
         public string Image { get; set; }
         public bool IsHost { get; set; }
