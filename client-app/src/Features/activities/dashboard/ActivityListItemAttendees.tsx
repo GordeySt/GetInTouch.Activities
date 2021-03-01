c8326101bdd4aec5412fce49a75e8b0af82d4130
gordeyst <gordeystahovets@gmail.com>
2021-03-01 21:19:51 +0300

    Add functionality to load attendees dynamically

--- client-app/src/Features/activities/dashboard/ActivityListItemAttendees.tsx
+++ client-app/src/Features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { List, Image } from "semantic-ui-react";
+import { List, Image, Popup } from "semantic-ui-react";
 import { IAttendee } from "../../../App/models/activity";
 
 interface IProps {
-    attendees: IAttendee[] | null;
+  attendees: IAttendee[] | null;
 }
 
 export const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
   return (
     <List horizontal>
-      <List.Item>
-        <Image size="mini" circular src="/assets/user.jpg" />
-      </List.Item>
-      <List.Item>
-        <Image size="mini" circular src="/assets/user.jpg" />
-      </List.Item>
-      <List.Item>
-        <Image size="mini" circular src="/assets/user.jpg" />
-      </List.Item>
+      {attendees?.map((attendee) => (
+        <List.Item key={attendee.username}>
+          <Popup
+            header={attendee.displayedName}
+            trigger={
+              <Image
+                size="mini"
+                circular
+                src={attendee.image || "/assets/user.jpg"}
+                style={{ cursor: "pointer " }}
+              />
+            }
+          />
+        </List.Item>
+      ))}
     </List>
   );
 };
