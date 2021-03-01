3d2ce00837452c6a0846af6f112c667e5d316ec9
gordeyst <gordeystahovets@gmail.com>
2021-03-01 21:07:45 +0300

    Add attendee interface

--- client-app/src/App/models/activity.ts
+++ client-app/src/App/models/activity.ts
@@ -6,6 +6,7 @@ export interface IActivity {
     date?: Date;
     city: string;
     venue: string;
+    attendees: IAttendee[] | null;
 }
 
 export class ActivityFormValues implements IActivity {
@@ -16,8 +17,16 @@ export class ActivityFormValues implements IActivity {
     date?: Date = undefined;
     city: string = "";
     venue: string = "";
+    attendees: IAttendee[] | null = null;
 
     constructor(init?: IActivity) {
         Object.assign(this, init);
     }
+}
+
+export interface IAttendee {
+    username: string;
+    displayedName: string;
+    image: string;
+    isHost: boolean;
 }
