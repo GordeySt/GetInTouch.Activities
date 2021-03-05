b8c9a30e7fba7ebe9e0d092ca05f054d3fd2813b
gordeyst <gordeystahovets@gmail.com>
2021-03-05 20:42:14 +0300

    Fix bug with unsupported media

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -8,6 +8,6 @@ namespace API.Controllers
     public class PhotosController : BaseController
     {
         [HttpPost]
-        public async Task<ActionResult<Photo>> AddPhoto(AddPhoto.Command command) => await Mediator.Send(command);
+        public async Task<ActionResult<Photo>> AddPhoto([FromForm]AddPhoto.Command command) => await Mediator.Send(command);
     }
 }
