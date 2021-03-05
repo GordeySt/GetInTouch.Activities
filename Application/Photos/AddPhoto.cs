2849a14d26dc7e2dadcee8c02a1304093b2dfebd
gordeyst <gordeystahovets@gmail.com>
2021-03-05 20:31:40 +0300

    Code refactoring

--- Application/Photos/AddPhoto.cs
+++ Application/Photos/AddPhoto.cs
@@ -33,26 +33,53 @@ namespace Application.Photos
 
             public async Task<Photo> Handle(Command request, CancellationToken cancellationToken)
             {
-                var photoUploadResult = _photoAccessor.AddPhoto(request.File);
+                var photoUploadResult = AddPhotoToCloud(request);
 
                 var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
-                
-                var photo = new Photo
-                {
-                    Url = photoUploadResult.Url,
-                    Id = photoUploadResult.publicId
-                };
 
-                if (!user.Photos.Any(x => x.IsMain)) 
-                    photo.IsMain = true;
+                var photo = CreateNewPhoto(photoUploadResult);
 
-                user.Photos.Add(photo);
+                CheckIfUserHasMainPhoto(user, photo);
+
+                AddPhotoToUserCollectionOfPhoto(user, photo);
 
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return photo;
 
                 throw new Exception("Problem saving changes");
+            }
+
+            private PhotoUploadResult AddPhotoToCloud(Command request)
+            {
+                return _photoAccessor.AddPhoto(request.File);
+            }
+
+            private Photo CreateNewPhoto(PhotoUploadResult photoUploadResult)
+            {
+                var photo = new Photo
+                {
+                    Url = photoUploadResult.Url,
+                    Id = photoUploadResult.publicId
+                };
+
+                return photo;
+            }
+
+            private void CheckIfUserHasMainPhoto(AppUser user, Photo photo)
+            {
+                if (!user.Photos.Any(x => x.IsMain))
+                    SetPhotoAsMain(photo);
+            }
+
+            private void SetPhotoAsMain(Photo photo)
+            {
+                photo.IsMain = true;
+            }
+
+            private void AddPhotoToUserCollectionOfPhoto(AppUser user, Photo photo)
+            {
+                user.Photos.Add(photo);
             }
         }
     }
