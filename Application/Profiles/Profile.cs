3ec05f79dfe57c0db664d36d0b943b2d17ee6f5a
gordeyst <gordeystahovets@gmail.com>
2021-03-05 21:04:14 +0300

    Add query for profile details

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -7,7 +7,7 @@ namespace Application.Profiles
     {
         public string DisplayedName { get; set; }
         public string UserName { get; set; }
-        public string Image { get; set; }
+        public string MainImage { get; set; }
         public string Bio { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
