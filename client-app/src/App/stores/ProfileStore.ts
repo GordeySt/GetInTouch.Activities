bccdfb10c400b84dd9907bf19097d073a38664ff
gordeyst <gordeystahovets@gmail.com>
2021-03-11 18:52:51 +0300

    Add buttons to delete, set as main and load photo

--- client-app/src/App/stores/ProfileStore.ts
+++ client-app/src/App/stores/ProfileStore.ts
@@ -1,6 +1,7 @@
-import { observable, action, makeAutoObservable, configure, runInAction } from "mobx"
+import { observable, action, makeAutoObservable, configure, runInAction, computed } from "mobx"
 import { Profiles } from "../api/agent";
 import { IProfile } from "../models/profile";
+import UserStore from "./UserStore";
 
 configure({ enforceActions: "always" });
 
@@ -10,6 +11,14 @@ class ProfileStore {
 
     constructor() {
         makeAutoObservable(this);
+    }
+
+    @computed get isCurrentUser() {
+        if (UserStore.user && this.profile) {
+            return UserStore.user.userName === this.profile.userName;
+        } else {
+            return false;
+        }
     }
 
     @action loadProfile = async (username: string) => {
--- client-app/src/Features/profiles/ProfilePhotos.tsx
+++ client-app/src/Features/profiles/ProfilePhotos.tsx
@@ -1,19 +1,45 @@
-import { Card, Header, Tab, Image } from "semantic-ui-react";
-import ProfileStore  from "../../App/stores/ProfileStore"
+import { useState } from "react";
+import { Card, Header, Tab, Image, Button, Grid } from "semantic-ui-react";
+import ProfileStore from "../../App/stores/ProfileStore";
 
 export const ProfilePhotos = () => {
-  const { profile } = ProfileStore;
+  const { profile, isCurrentUser } = ProfileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
   return (
     <Tab.Pane>
-      <Header icon="image" content="photos">
-        <Card.Group itemsPerRow={5}>
-            {profile && profile.photos.map((photo) => (
-              <Card key={photo.id}>
-                <Image src={photo.url} />
-              </Card>
-            ))}
-        </Card.Group>
-      </Header>
+      <Grid>
+        <Grid.Column width={16} style={{ paddingBottom: "0" }}>
+          <Header floated="left" icon="image" content="Photos" />
+          {isCurrentUser && (
+            <Button
+              floated="right"
+              icon={addPhotoMode ? "cancel" : "photo"}
+              basic
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <p>Photo Widget</p>
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile &&
+                profile.photos.map((photo) => (
+                  <Card key={photo.id}>
+                    <Image src={photo.url} />
+                    {isCurrentUser && (
+                      <Button.Group fluid width={2}>
+                        <Button colour="gray" content="Main" />
+                        <Button color="black" icon="trash" />
+                      </Button.Group>
+                    )}
+                  </Card>
+                ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
     </Tab.Pane>
   );
 };
