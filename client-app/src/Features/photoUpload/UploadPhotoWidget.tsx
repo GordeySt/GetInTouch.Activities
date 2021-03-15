7e43905e21549a6fd5ad4dfbed9cb34f45f22da9
gordeyst <gordeystahovets@gmail.com>
2021-03-16 00:20:07 +0300

    Fix

--- client-app/src/Features/photoUpload/UploadPhotoWidget.tsx
+++ client-app/src/Features/photoUpload/UploadPhotoWidget.tsx
@@ -1,27 +1,24 @@
 import { Fragment } from "react";
-import { Grid, Header, Segment } from "semantic-ui-react";
+import { Grid, Header } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { UploadPhotoDropzone } from "./UploadPhotoDropzone";
 
 export const UploadPhotoWidget = observer(() => {
   return (
     <Fragment>
-      <Segment>
-        <Grid>
-          <Grid.Row />
-          <Grid.Column width={4}>
-            <Header color="teal" sub content="Step 1 - Add Photo" />
-          </Grid.Column>
-          <Grid.Column width={1} />
-          <Grid.Column width={4}>
-            <Header sub color="teal" content="Step 2 - Resize image" />
-          </Grid.Column>
-          <Grid.Column width={1} />
-          <Grid.Column width={4}>
-            <Header sub color="teal" content="Step 3 - Preview & Upload" />
-          </Grid.Column>
-        </Grid>
-      </Segment>
+      <Grid>
+        <Grid.Column width={4}>
+          <Header color="teal" sub content="Step 1 - Add Photo" />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 2 - Resize image" />
+        </Grid.Column>
+        <Grid.Column width={1} />
+        <Grid.Column width={4}>
+          <Header sub color="teal" content="Step 3 - Preview & Upload" />
+        </Grid.Column>
+      </Grid>
     </Fragment>
   );
 });
