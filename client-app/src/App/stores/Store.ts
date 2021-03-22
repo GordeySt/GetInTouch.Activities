14be4b619e75966fe183893d451d3f530339a7c4
gordeyst <gordeystahovets@gmail.com>
2021-03-22 13:03:16 +0300

    Add profile store to the store

--- client-app/src/App/stores/Store.ts
+++ client-app/src/App/stores/Store.ts
@@ -1,18 +1,21 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./ActivityStore";
 import CommonStore from "./CommonStore";
+import ProfileStore from "./ProfileStore";
 import UserStore from "./UserStore";
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
     userStore: UserStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
-    userStore: new UserStore()
+    userStore: new UserStore(),
+    profileStore: new ProfileStore()
 }
 
 export const StoreContext = createContext(store);
