822948cd2a8373765a1e119972aab244ea852179
gordeyst <gordeystahovets@gmail.com>
2021-03-25 22:26:18 +0300

    Remove IChecker service

--- API/Extensions/UserServicesExtensions.cs
+++ API/Extensions/UserServicesExtensions.cs
@@ -1,5 +1,4 @@
 using Application.Interfaces;
-using Infrastructure.Common;
 using Infrastructure.Photos;
 using Infrastructure.Security;
 using Microsoft.AspNetCore.Authorization;
@@ -16,7 +15,6 @@ namespace API.Extensions
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
-            services.AddScoped<IChecker, Checker>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
--- Application/Interfaces/IChecker.cs
+++
@@ -1,9 +0,0 @@
-using Domain;
-
-namespace Application.Interfaces
-{
-    public interface IChecker
-    {
-        void checkIfActivityNotFound(Activity activity);
-    }
-}
