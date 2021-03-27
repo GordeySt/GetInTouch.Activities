20656071a588824474a8ced35104969432bda826
gordeyst <gordeystahovets@gmail.com>
2021-03-27 09:17:54 +0300

    Refactor photo controller

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -12,9 +12,9 @@ namespace API.Controllers
         public async Task<ActionResult<Photo>> AddPhoto([FromForm] AddPhoto.Command command) => await Mediator.Send(command);
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> DeletePhoto(string id) => await Mediator.Send(new DeletePhoto.Command { ImageId = id });
+        public async Task<IActionResult> DeletePhoto(string id) => Ok(await Mediator.Send(new DeletePhoto.Command { ImageId = id }));
 
         [HttpPost("{id}/setmain")]
-        public async Task<ActionResult<Unit>> SetMain(string id) => await Mediator.Send(new SetMainPhoto.Command { Id = id });
+        public async Task<IActionResult> SetMain(string id) => Ok(await Mediator.Send(new SetMainPhoto.Command { Id = id }));
     }
 }
