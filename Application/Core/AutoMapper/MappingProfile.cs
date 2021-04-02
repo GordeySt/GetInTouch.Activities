58f8ce4c3c091e75a31008c38f7d5641b7d17bf8
gordeyst <gordeystahovets@gmail.com>
2021-04-03 01:28:49 +0300

    Refactor mapping profile

---
+++ Application/Core/AutoMapper/MappingProfile.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Application.Activities;
+using Application.Comments.Dto;
+using AutoMapper;
+using Domain;
+
+namespace Application.Core.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Activity, ActivityDto>();
+            CreateMap<UserActivity, AttendeeDto>()
+                .ForMember(d => d.UserName, options => options.MapFrom(source => source.AppUser.UserName))
+                .ForMember(d => d.DisplayedName, options => options.MapFrom(source => source.AppUser.DisplayedName))
+                .ForMember(d => d.Image, options => options.MapFrom(source => source.AppUser.Photos.FirstOrDefault(x =>
+                x.IsMain).Url));
+            CreateMap<Comment, CommentDto>()
+               .ForMember(d => d.UserName, o => o.MapFrom(s => s.Author.UserName))
+               .ForMember(d => d.DisplayedName, o => o.MapFrom(s => s.Author.DisplayedName))
+               .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x
+               => x.IsMain).Url));
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.MainImage, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count()))
+                .ForMember(d => d.FollowingsCount, o => o.MapFrom(s => s.Followings.Count()));
+        }
+    }
+}
