63c6d699ca8e1159d0cecad8d740d7da1e08e1b4
gordeyst <gordeystahovets@gmail.com>
2021-04-03 01:14:02 +0300

    Add new endpoint for getting following/followers

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -11,5 +11,15 @@ namespace API.Controllers
         {
             TargetName = username
         }));
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new FollowList.Query
+            {
+                UserName = username,
+                Predicate = predicate
+            }));
+        }
     }
 }
