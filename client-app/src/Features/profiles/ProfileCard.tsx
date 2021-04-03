2ac047932e1f1d9bafe9aab79b341af43fd88fef
gordeyst <gordeystahovets@gmail.com>
2021-04-03 16:03:33 +0300

    Add profile card with short profile info

---
+++ client-app/src/Features/profiles/ProfileCard.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { Card, Icon, Image } from "semantic-ui-react";
+import { IAttendee } from "../../App/models/activity";
+
+interface IProps {
+  profile: IAttendee;
+}
+
+export const ProfileCard: React.FC<IProps> = ({ profile }) => {
+  const truncate = (str: string | undefined) => {
+    if (str) {
+      return str.length > 40 ? str.substring(0, 37) + "..." : str;
+    }
+  };
+
+  return (
+    <Card as={Link} to={`/profiles/${profile?.userName}`}>
+      <Image src={profile?.mainImage || "/assets/user.jpg"} />
+      <Card.Content>
+        <Card.Header>{profile?.displayedName}</Card.Header>
+        <Card.Description>{truncate(profile?.bio)}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="user" />
+        {profile?.followersCount} followers
+      </Card.Content>
+    </Card>
+  );
+};
