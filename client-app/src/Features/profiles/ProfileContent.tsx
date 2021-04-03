ec96bdc9233767639ee52bcf8fea369967a89140
gordeyst <gordeystahovets@gmail.com>
2021-04-03 17:40:07 +0300

    Add followings component to tabs

--- client-app/src/Features/profiles/ProfileContent.tsx
+++ client-app/src/Features/profiles/ProfileContent.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 import { Tab } from "semantic-ui-react";
 import { ProfilePhotos } from "./ProfilePhotos";
+import { ProfileFollowings } from "./ProfileFollowings";
 
 const panes = [
   { menuItem: "About", render: () => <Tab.Pane>About</Tab.Pane> },
   { menuItem: "Photos", render: () => <ProfilePhotos /> },
   { menuItem: "Activities", render: () => <Tab.Pane>Activities</Tab.Pane> },
-  { menuItem: "Followers", render: () => <Tab.Pane>Followers</Tab.Pane> },
-  { menuItem: "Following", render: () => <Tab.Pane>Following</Tab.Pane> },
+  { menuItem: "Followers", render: () => <ProfileFollowings /> },
+  { menuItem: "Following", render: () => <ProfileFollowings /> },
 ];
 
 export const ProfileContent = () => {
