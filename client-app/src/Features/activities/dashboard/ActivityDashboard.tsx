5b5d5616f55ffe839ee2b175662769e45ad7fc1d
gordeyst <gordeystahovets@gmail.com>
2021-04-05 21:29:53 +0300

    Add pagination to the client

--- client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
@@ -1,23 +1,49 @@
-import React, { useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Button, Grid } from "semantic-ui-react";
 import { ActivityList } from "./ActivityList";
 import { observer } from "mobx-react-lite";
-import { store, useStore } from "../../../App/stores/Store";
+import { useStore } from "../../../App/stores/Store";
 import { LoadingComponent } from "../../../App/layout/LoadingComponent";
 import { ActivityFilters } from "./ActivityFilters";
+import { PagingParams } from "../../../App/models/pagination";
 
 export const ActivityDashboard: React.FC = observer(() => {
   const { activityStore } = useStore();
-  useEffect(() => {
-    store.activityStore.loadActivities();
-  }, [activityStore]);
+  const {
+    loadActivities,
+    activitiesRegistry,
+    setPagingParams,
+    pagination,
+    loadingInitial,
+  } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
 
-  if (store.activityStore.loadingInitial) return <LoadingComponent />;
+  const handleGetNext = () => {
+    setLoadingNext(true);
+    if (pagination) {
+      setPagingParams(new PagingParams(pagination.currentPage + 1));
+    }
+    loadActivities().then(() => setLoadingNext(false));
+  };
+
+  useEffect(() => {
+    if (activitiesRegistry.size <= 1) loadActivities();
+  }, [activitiesRegistry.size, loadActivities]);
+
+  if (loadingInitial && !loadingNext) return <LoadingComponent />;
 
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
+        <Button
+          floated="right"
+          content="More..."
+          positive
+          onClick={handleGetNext}
+          loading={loadingNext}
+          disabled={pagination?.totalPages === pagination?.currentPage}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
         <ActivityFilters />
