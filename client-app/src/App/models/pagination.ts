07dc7bbe8dffaccdcb14dd244addddbec3b02e6a
gordeyst <gordeystahovets@gmail.com>
2021-04-05 20:53:56 +0300

    Add interfaces and classes for pagination

---
+++ client-app/src/App/models/pagination.ts
@@ -0,0 +1,21 @@
+export interface IPagination {
+    currentPage: number;
+    itemsPerPage: number;
+    totalItems: number;
+    totalPages: number;
+}
+
+export class PaginatedResult<T> {
+    data: T;
+    pagination: IPagination
+
+    constructor(data: T, pagination: IPagination) {
+        this.data = data;
+        this.pagination = pagination;
+    }
+}
+
+export class PagingParams {
+    pageNumber = 1;
+    pageSize = 2;
+}
