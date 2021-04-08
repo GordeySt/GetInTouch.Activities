496b83b5e3f085bcecf1c0096038fe21828ef408
gordeyst <gordeystahovets@gmail.com>
2021-04-08 21:50:22 +0300

    Add controller for fb login

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,12 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Application.User;
+using Domain;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace API.Controllers
 {
     public class UserController : BaseApiController
     {
+        private readonly IConfiguration _config;
+        private readonly HttpClient _httpClient;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IJwtGenerator _jwtGenerator;
+
+        public UserController(UserManager<AppUser> userManager, IConfiguration config,
+            IJwtGenerator jwtGenerator)
+        {
+            _jwtGenerator = jwtGenerator;
+            _userManager = userManager;
+            _config = config;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new System.Uri("https://graph.facebook.com")
+            };
+        }
+
         [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
@@ -14,17 +40,76 @@ namespace API.Controllers
             return await Mediator.Send(query);
         }
 
-        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Register.Command command)
         {
             return await Mediator.Send(command);
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<User>> CurrentUser()
         {
             return await Mediator.Send(new CurrentUser.Query());
+        }
+
+        [AllowAnonymous]
+        [HttpPost("fbLogin")]
+        public async Task<ActionResult<User>> FacebookLogin(string accessToken)
+        {
+            var fbVerifyKeys = _config["Facebook:AppId"] + "|" + _config["Facebook:AppSecret"];
+
+            var verifyToken = await _httpClient
+                .GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKeys}");
+
+            if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
+
+            var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
+
+            var response = await _httpClient.GetAsync(fbUrl);
+
+            if (!response.IsSuccessStatusCode) return Unauthorized();
+
+            var fbInfo = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+
+            var username = (string)fbInfo.id;
+
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user != null) return CreateUserObject(user);
+
+            user = new AppUser
+            {
+                DisplayedName = (string)fbInfo.name,
+                Email = (string)fbInfo.email,
+                UserName = (string)fbInfo.id,
+                Photos = new List<Photo>
+            {
+                new Photo
+                {
+                    Id = "fb_" + (string)fbInfo.id,
+                    Url = (string)fbInfo.picture.data.url,
+                    IsMain = true
+                }}
+            };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded) return BadRequest("Problem creating user account");
+
+            return CreateUserObject(user);
+        }
+
+        private User CreateUserObject(AppUser user)
+        {
+            return new User
+            {
+                DisplayedName = user.DisplayedName,
+                UserName = user.UserName,
+                Token = _jwtGenerator.CreateToken(user),
+                Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            };
         }
     }
 }
