3bafd6047f03a698a1f79c484cdf8be93fec4b7f
gordeyst <gordeystahovets@gmail.com>
2021-04-08 17:58:57 +0300

    Add endpoint within controller for getting profile activities

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -11,5 +11,16 @@ namespace API.Controllers
         {
             return await Mediator.Send(new ProfileDetails.Query { UserName = username });
         }
+
+        [HttpGet("{username}/activities")]
+        public async Task<IActionResult> GetUserActivities(string username,
+            string predicate)
+        {
+            return HandleResult(await Mediator.Send(new ProfileActivities.Query
+            {
+                UserName = username,
+                Predicate = predicate
+            }));
+        }
     }
 }
