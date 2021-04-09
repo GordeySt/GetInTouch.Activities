0b25ba3dcb3ae209ecb4303e3879903fe9b99ded
gordeyst <gordeystahovets@gmail.com>
2021-04-09 11:31:05 +0300

    Add refresh token property

--- Application/User/User.cs
+++ Application/User/User.cs
@@ -1,10 +1,27 @@
+using System.Linq;
+using System.Text.Json.Serialization;
+using Application.Interfaces;
+using Domain;
+
 namespace Application.User
 {
     public class User
     {
+        public User(AppUser user, IJwtGenerator jwtGenerator, string refreshToken)
+        {
+            DisplayedName = user.DisplayedName;
+            Token = jwtGenerator.CreateToken(user);
+            UserName = user.UserName;
+            Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url;
+            RefreshToken = refreshToken;
+        }
+
         public string DisplayedName { get; set; }
         public string Token { get; set; }
         public string UserName { get; set; }
         public string Image { get; set; }
+
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
     }
 }
